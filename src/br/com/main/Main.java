f61720d5778982fc0ccd81b3aae40f832ce5de35
Tennison Capra <tennison_capra@hotmail.com>
2022-01-27 13:00:44 -0300

    minor changes

--- src/br/com/main/Main.java
+++ src/br/com/main/Main.java
@@ -7,21 +7,31 @@ package br.com.main;
 public class Main {
     public static void main(String[] args){
         Banco santander = new Banco("Santander");
-        
+        Banco bradesco = new Banco("Bradesco");
+
         Cliente tete = new Cliente("Tete");
         Conta cc1 = new ContaCorrente(tete, santander);
         cc1.depositar(1350d);
 
         Cliente juca = new Cliente("Juca");
-        Conta cc2 = new ContaPoupanca(juca, santander);
+        Conta cc2 = new ContaPoupanca(juca, bradesco);
         cc2.depositar(1800d);
 
-       cc1.imprimirExtrato();
-       cc2.imprimirExtrato();
+        cc1.imprimirExtrato();
+        cc2.imprimirExtrato();
 
-       cc1.transferir(200d, cc2);
+        cc1.transferir(200d, cc2);
 
+        cc1.imprimirExtrato();
+        cc2.imprimirExtrato();
 
+        /*
+            Ideias de melhoria:
+            - Não permitir instâncias de banco com o mesmo nome.
+            - Ao instanciar uma nova conta, verificar qual foi o último número de conta daquele banco em específico
+            e incrementar o número de conta apenas no grupo daquele banco.
+
+         */
 
     }
 }
