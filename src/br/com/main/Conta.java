ca4ebf9df259d40b549a7aaaa3e50694d313cca0
Tennison Capra <tennison_capra@hotmail.com>
2022-01-27 12:32:03 -0300

    more improvements

--- src/br/com/main/Conta.java
+++ src/br/com/main/Conta.java
@@ -17,17 +17,17 @@ public abstract class Conta implements InterfaceConta {
     @Override
     public void sacar(double valor) {
         if(valor > this.getSaldo()){
-            System.out.println("Não é possível sacar essa quantia.\nSaldo insuficiente!\n\n");
+            System.out.printf("Olá, %s! Não é possível sacar essa quantia.\nSaldo insuficiente!\n\n", this.cliente.getNome());
         }else{
             this.setSaldo(this.getSaldo() - valor);
-            System.out.printf("Você sacou R$%.2f.\nSeu novo saldo é R$%.2f.\n\n", valor, this.getSaldo());
+            System.out.printf("Olá, %s! Você sacou R$%.2f.\nSeu novo saldo é R$%.2f.\n\n", this.cliente.getNome(), valor, this.getSaldo());
         }
     }
 
     @Override
     public void depositar(double valor) {
         this.setSaldo(this.getSaldo() + valor);
-        System.out.printf("Você depositou R$%.2f.\nSeu novo saldo é R$%.2f.\n\n", valor, this.getSaldo());
+        System.out.printf("Olá, %s! Você depositou R$%.2f.\nSeu novo saldo é R$%.2f.\n\n", this.cliente.getNome(), valor, this.getSaldo());
     }
 
     @Override
