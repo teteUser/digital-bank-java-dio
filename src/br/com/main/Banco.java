76dfe5fe13df0427d889debf4b40d1ada849e223
Tennison Capra <tennison_capra@hotmail.com>
2022-01-30 11:08:34 -0300

    minor changes

--- src/br/com/main/Banco.java
+++ src/br/com/main/Banco.java
@@ -13,6 +13,12 @@ public class Banco {
 
     public Banco(String nome) {
         this.nome = nome;
+        boolean rep = false;
+        Iterator<Set> it = bancos.iterator();
+        while(it.hasNext()){
+            Set<Banco> b = new HashSet<>();
+            b = it.next();
+        }
         this.bancos.add(this);
     }
 
